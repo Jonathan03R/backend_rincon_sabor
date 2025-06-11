6b2a9299bc55af97732cf823dd1421f3ea0cfaf8
Jonathan Roque Gonzales <jonathanjesusroquegonzales@gmail.com>
2025-06-11 15:16:37 -0500

    feature: implementancion de webSocket para sabewr en tiempo real el cambio de las mesas y otro enpoint

--- routes/mesas.js
+++ routes/mesas.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const { poolPromise } = require('../config/connection');
+const { emitirActualizacionMesas } = require('../sockets/mesasSocket');
 
 //consstantes creadas para los procedimientos almacenados
 
 const SP_OBTENER_MESAS = 'Pedidos.Proc_ObtenerMesas';
+const SP_CAMBIAR_ESTADO_MESA = 'Pedidos.Proc_CambiarEstadoMesa';
 
 const router = express.Router();
 
@@ -28,6 +30,32 @@ router.get('/obtener', async (req, res) => {
     });
   }
 });
+
+
+router.put('/actualizar', async (req, res) => {
+  try {
+    const { MesaCodigo, nuevoEstado } = req.body;
+
+    if (!MesaCodigo || !nuevoEstado) {
+      return res.status(400).json({ success: false, message: 'Parámetros incompletos' });
+    }
+
+    const pool = await poolPromise;
+    await pool.request()
+      .input('MesaCodigo', MesaCodigo)
+      .input('nuevoEstado', nuevoEstado)
+      .execute(SP_CAMBIAR_ESTADO_MESA);
+
+    // 🔊 Emitimos la actualización a todos los clientes
+    emitirActualizacionMesas();
+
+    res.status(200).json({ success: true, message: 'Estado actualizado correctamente' });
+  } catch (error) {
+    console.error('Error al actualizar estado de mesa:', error);
+    res.status(500).json({ success: false, message: 'Error del servidor' });
+  }
+});
+
 
 
 module.exports = router;
--- utils/cloudinaryHelper.js
+++ utils/cloudinaryHelper.js
@@ -1,12 +1,16 @@
 const cloudinary = require('../config/cloudinary');
 
 const uploadImageToCloudinary = async (file) => {
-    const buffer = file.buffer;
-    const base64 = buffer.toString('base64');
+  console.log('Subiendo imagen, tamaño en buffer:', file.buffer?.length);
+  try {
+    const base64 = file.buffer.toString('base64');
     const dataUri = `data:${file.mimetype};base64,${base64}`;
-
     const result = await cloudinary.uploader.upload(dataUri);
     return result.secure_url;
+  } catch (err) {
+    console.error('Error en Cloudinary:', err);
+    throw err;
+  }
 };
 
 module.exports = { uploadImageToCloudinary };
